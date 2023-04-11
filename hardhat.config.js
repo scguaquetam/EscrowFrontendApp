a50b4ba3c6dc6e20ecacb3a14de2e266e311a81a
Sebas <sheva3232@gmail.com>
2023-04-10 21:06:07 -0500

    starting

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,15 @@
 require('@nomicfoundation/hardhat-toolbox');
+require('dotenv').config();
 
 module.exports = {
   solidity: "0.8.17",
   paths: {
     artifacts: "./app/src/artifacts",
+  },
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.INFURA_API_KEY}`,
+      accounts: [process.env.SEPOLIA_PRIVATE_KEY]
+    }
   }
 };
